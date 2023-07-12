dcd5c2edb198822e637ca8c961c5b7c490cfca29
kansascity1 <139292554+kansascity1@users.noreply.github.com>
2023-07-11 19:17:11 -0500

    Update swinstall.ps1
    Added new MSI command switches per:
    https://www.sonicwall.com/support/knowledge-base/commands-for-silent-installation-of-netextender/221116130013937/

--- App Configurations/SonicWall NetExtender/swinstall.ps1
+++ App Configurations/SonicWall NetExtender/swinstall.ps1
@@ -14,6 +14,7 @@
 
 ### VARIABLES ###
 $app = "C:\Program Files (x86)\SonicWall\SSL-VPN\NetExtender\NEGui.exe"
+# Need to change below to match GUID of new MSI
 $software = "{EF06A6A8-6B81-4A09-8223-789953972FFF}"
 $version = (Get-ItemPropertyValue HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$software -Name DisplayVersion -ErrorAction SilentlyContinue)
 ### VARIABLES ###
@@ -27,7 +28,7 @@ if (-not(Test-Path -Path $app -PathType Leaf)) {
     New-Item -ItemType Directory -Force -Path "C:\MDM\SonicWallNetExtender" | Out-Null
 
     # Copies NetExtender.msi to newly created folder
-    Copy-Item -Path "$PSScriptRoot\NetExtender-10.2.315.msi" -Destination "C:\MDM\SonicWallNetExtender\NetExtender-10.2.315.msi"
+    Copy-Item -Path "$PSScriptRoot\netextender-x64-10.2.331.msi" -Destination "C:\MDM\SonicWallNetExtender\netextender-x64-10.2.331.msi"
 
     #Copies profile.ps1 to newly created folder
     Copy-Item -Path "$PSScriptRoot\profile.ps1" -Destination "C:\MDM\SonicWallNetExtender\profile.ps1"
@@ -36,7 +37,7 @@ if (-not(Test-Path -Path $app -PathType Leaf)) {
     Copy-Item -Path "$PSScriptRoot\Create_Profile.bat" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\Create_Profile.bat"
 
     #This installs SonicWall NetExtender. Note, this does a restart! If you perform /norestart the file will not install correct.
-    msiexec /i "C:\MDM\SonicWallNetExtender\NetExtender-10.2.315.msi" /q 
+    msiexec.exe /i "C:\MDM\SonicWallNetExtender\netextender-x64-10.2.331.msi" /norestart /qn 
     }
 # If SonicWall NetExtender App does exist, check if version meets minimum requirements
 elseif ($version -le 10.1) {
@@ -44,7 +45,7 @@ elseif ($version -le 10.1) {
     New-Item -ItemType Directory -Force -Path "C:\MDM\SonicWallNetExtender" | Out-Null
 
     # Copies NetExtender.msi to newly created folder
-    Copy-Item -Path "$PSScriptRoot\NetExtender-10.2.315.msi" -Destination "C:\MDM\SonicWallNetExtender\NetExtender-10.2.315.msi"
+    Copy-Item -Path "$PSScriptRoot\netextender-x64-10.2.331.msi" -Destination "C:\MDM\SonicWallNetExtender\netextender-x64-10.2.331.msi"
 
     #Copies profile.ps1 to newly created folder
     Copy-Item -Path "$PSScriptRoot\profile.ps1" -Destination "C:\MDM\SonicWallNetExtender\profile.ps1"
@@ -56,6 +57,7 @@ elseif ($version -le 10.1) {
     Copy-Item -Path "$PSScriptRoot\runinstall.bat" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\runinstall.bat" 
 
     #Removes NetExtender by Product code. This removes application and registry files. 
+    # Need to change below to match GUID of new MSI
     msiexec /x "{EF06A6A8-6B81-4A09-8223-789953972FFF}" /q 
     }
 else {
